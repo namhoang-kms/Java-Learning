9b6d8b0016588cf7ea7947e1b16b35f291c2ee26
thaochiclair <155962475+thaochiclair@users.noreply.github.com>
2024-04-29 14:53:36 +0700

    Add files via upload

---
+++ print-12-month-elseif-switch/src/elseifprint12month.java
@@ -0,0 +1,33 @@
+public class elseifprint12month {
+
+        public static void main(String[] args) {
+            byte clairMonth = -128;
+            if (clairMonth == 1) {
+                System.out.println("Hello January my Tet++ <3 ");
+            } else if (clairMonth == 2) {
+                System.out.println("Hi Feb it's gonna be Lunar New Year soon <3 ");
+            } else if (clairMonth == 3) {
+                System.out.println("Month of romantic boys & girls");
+            } else if (clairMonth == 4) {
+                System.out.println("Thang cua Be' B0ng? :hands-heart ");
+            } else if (clairMonth == 5) {
+                System.out.println("Thang nam hay la thang nam :))");
+            } else if (clairMonth == 6) {
+                System.out.println("Mua thang 6 di DL an sinh nhat Thao Guynh hoy :>");
+            } else if (clairMonth == 7) {
+                System.out.println("chac la Sg nong vl can dua tac hoac da me song sot qua ngay ah ");
+            } else if (clairMonth == 8) {
+                System.out.println("Thang 8 co gi hay khum do?");
+            } else if (clairMonth == 9) {
+                System.out.println("May qua het di hoc oy nhung lai di lam = khai giang ca nam :'( ");
+            } else if (clairMonth == 10) {
+                System.out.println("Be' ve an sing nhat phu hoang mau hau cong tua Nep hoyyy \n cau mong ve may re nhu beo gium con ah :>>");
+            } else if (clairMonth == 11) {
+                System.out.println("Tang toc cay cuoc nao sap het nam uiii >~<!");
+            } else if (clairMonth == 12) {
+                System.out.println("Co duoc di dau choi khum taaaa");
+            } else {
+                System.out.println("Pick randomly a number from 1 to 12 to receive a message from my heart hi' hi' :>  ");
+            }
+        }
+}
---
+++ print-12-month-elseif-switch/src/switchcaseprint12month.java
@@ -0,0 +1,47 @@
+public class switchcaseprint12month {
+
+    public static void main(String[] args) {
+        byte clairMonth = -111;
+        switch (clairMonth) {
+            case 1:
+                System.out.println("Hello January my Tet++ <3 ");
+                break;
+            case 2:
+                System.out.println("Hi Feb it's gonna be Lunar New Year soon <3 ");
+                break;
+            case 3:
+                System.out.println("Month of romantic boys & girls");
+                break;
+            case 4:
+                System.out.println("Thang cua Be' B0ng? :hands-heart ");
+                break;
+            case 5:
+                System.out.println("Thang nam hay la thang nam :))");
+                break;
+            case 6:
+                System.out.println("Mua thang 6 di DL an sinh nhat Thao Guynh hoy :>");
+                break;
+            case 7:
+                System.out.println("chac la Sg nong vl can dua tac hoac da me song sot qua ngay ah ");
+                break;
+            case 8:
+                System.out.println("Thang 8 co gi hay khum o?");
+                break;
+            case 9:
+                System.out.println("May qua het di hoc oy nhung lai di lam = khai giang ca nam :'( ");
+                break;
+            case 10:
+                System.out.println("Be' ve an sing nhat phu hoang mau hau cong tua Nep hoyyy \n cau mong ve may re nhu beo gium con ah :>>");
+                break;
+            case 11:
+                System.out.println("Tang toc cay cuoc nao sap het nam uiii >~<!");
+                break;
+            case 12:
+                System.out.println("Co duoc di dau choi khum taaaa");
+                break;
+            default:
+                System.out.println("Pick randomly a number from 1 to 12 to receive a message from my heart hi' hi' :>  ");
+        }
+    }
+}
+
