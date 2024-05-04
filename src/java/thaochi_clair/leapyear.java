99ba68aed4a42e610c7f230d7d191c69cc664b8d
thaochiclair <155962475+thaochiclair@users.noreply.github.com>
2024-05-04 15:05:09 +0700

    Restructure folder (#6)
    Co-authored-by: Clair <ching@joblogic.com>

---
+++ src/java/thaochi_clair/leapyear.java
@@ -0,0 +1,15 @@
+package thaochi_clair.src.java.thaochi_clair;
+
+public class leapyear {
+    public static void main (String[] args) {
+        int clairYear = 2024;
+        if (clairYear%4 == 0) {
+            System.out.println("Ố năm nay năm nhuận ớ");
+        } else if (clairYear%4 == 1 || clairYear%4 == 2 || clairYear%4 ==3) {
+            System.out.println("nam nay nam thuong 365 ngay hoy ne`");
+        } else {
+            System.out.println("Nhập năm zô để test xem tui tính năm nhuận có chuẩn hăm nạh ");
+        }
+    }
+    
+}
